ad79f6613c7313fc23328aede46e8b16fb65f270
skytorin <nick555@bk.ru>
2021-06-07 18:28:11 +0300

    add key pair 07.06.2021 18-28-00

--- Lesson_03/lesson_03.tf
+++ Lesson_03/lesson_03.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "Server" {
     ami = "ami-043097594a7df80ec"      # Amazon Linux AMI
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.My_Firewall_Web.id]
+    key_name = "Skytorin"
     user_data = <<EOF
 #!/bin/bash
 yum -y update
@@ -26,6 +27,13 @@ EOF
 resource "aws_security_group" "My_Firewall_Web" {
   name        = "My_Firewall_Web"
   description = "My First Security Group"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port        = 80
@@ -51,5 +59,11 @@ resource "aws_security_group" "My_Firewall_Web" {
   tags = {
     Name = "Web Server Security Group"
     Owner = "Ostin"
- }
+  }
 }
+# Создание ключевой пары (Key pairs)
+#resource "aws_key_pair" "deployer" {
+#  key_name   = "My_Desktop_Key"
+#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAlnCgUbMjrQBRaB7PceXSr9UEE3Q+lxlPEzdY5UKwGA5sBjcSOlmuIh951nleZV874A7NdNp8dcQ+epdpFTkTOlylFSzsahF0miZyMUqz9vNBbFzfP53vbsN9yXe8ohkSjSD9Q35Kod0wiOpqrERS5rlX8YBEObUHEQexnTr7mXBe/T1/tEeYDUO+niQ07GOaXw5sXo2Frx/5NC8g6onyPMs80/8j8LFLbrv1fS74DqNM8G3CBb7GMxn1GEHRVke3dpISNZG7Wgf5ZP2DxsHYZwcgjpn1UbC9F1gN69QjwSf+CSL2Q4MkCnWNy9K+w+/wEksEDcxrS8h+InxnoGwaPQ== Ostin"
+#}
+
