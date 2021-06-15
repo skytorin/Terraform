4d59d2b10bed19bf87cbd26c1c247780cb6e429c
skytorin <nick555@bk.ru>
2021-06-15 22:36:49 +0300

    Added Lesson_04 15.06.2021 22-36-00

---
+++ Lesson_04/lesson_04.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+    profile = "terraform"
+    region = "eu-central-1"
+}
+
+resource "aws_instance" "Server" {
+    ami = "ami-05f7491af5eef733a"       # Ubuntu Linux AMI
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.My_Firewall_Web_2.id]
+    key_name = "Skytorin"
+    tags = {
+       Name = "Wiki Server"
+       Owner = "Ostin"
+       Project = "Lesson-04"
+ }
+}
+
+resource "aws_security_group" "My_Firewall_Web_2" {
+  name        = "My_Firewall_Web_2"
+  description = "My Two Security Group_"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "Web Server Security Group"
+    Owner = "Ostin"
+  }
+}
